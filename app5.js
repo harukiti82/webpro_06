000635f1f26e84df38c7db38b2ff33f9e932883f
Numata Haru <harukiti08@outlook.jp>
2025-11-14 11:18:58 +0900

    だい8回

--- app5.js
+++ app5.js
@@ -4,6 +4,20 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
 
+let station = [
+  { id:1, code:"JE01", name:"東京駅"},
+  { id:2, code:"JE07", name:"舞浜駅"},
+  { id:3, code:"JE12", name:"新習志野駅"},
+  { id:4, code:"JE13", name:"幕張豊砂駅"},
+  { id:5, code:"JE14", name:"海浜幕張駅"},
+  { id:6, code:"JE05", name:"新浦安駅"},
+];
+
+app.get("/keiyo", (req, res) => {
+  // 本来ならここにDBとのやり取りが入る
+  res.render('db1', { data: station });
+});
+
 app.get("/hello1", (req, res) => {
   const message1 = "Hello world";
   const message2 = "Bon jour";
@@ -47,11 +61,43 @@ app.get("/janken", (req, res) => {
   if( num==1 ) cpu = 'グー';
   else if( num==2 ) cpu = 'チョキ';
   else cpu = 'パー';
+  
   // ここに勝敗の判定を入れる
   // 以下の数行は人間の勝ちの場合の処理なので，
   // 判定に沿ってあいこと負けの処理を追加する
-  judgement = '勝ち';
-  win += 1;
+  if(hand ==  'グー')
+    if(cpu == 'グー')
+      judgement = 'あいこ';
+    ;
+    if(cpu == 'チョキ')
+      judgement = '勝ち';
+    ;
+    if(cpu == 'パー')
+      judgement = '負け';
+    ;
+  ;
+  if(hand ==  'チョキ')
+    if(cpu == 'チョキ')
+      judgement = 'あいこ';
+    ;
+    if(cpu == 'グー')
+      judgement = '負け';
+    ;
+    if(cpu == 'パー')
+      judgement = '勝ち';
+    ;
+  ;
+  if(hand ==  'パー')
+    if(cpu == 'パー')
+      judgement = 'あいこ';
+    ;
+    if(cpu == 'グー')
+      judgement = '勝ち';
+    ;
+    if(cpu == 'チョキ')
+      judgement = '負け';
+    ;
+  ;
   total += 1;
   const display = {
     your: hand,
