fbfe67ff285db809ada3eee22eae2ad9742fae77
Numata Haru <harukiti08@outlook.jp>
2025-12-25 21:33:52 +0900

    ポケモン図鑑機能の削除機能以外の作成

--- app5.js
+++ app5.js
@@ -24,6 +24,20 @@ let station2 = [
   { id:7, code:"JE18", name:"蘇我駅", change:"内房線，外房線", passengers:31328, distance:43.0 },
 ];
 
+let pokemon = [
+  { id:1, code:37, name:"ロコン",  bunrui:"きつねポケモン", type:"炎", takasa:0.6, omosa:9.9, setumei:"自分より強い相手に襲われると傷ついたふりをして惑わせその隙に逃げてしまう。" },
+  { id:2, code:56, name:"マンキー",  bunrui:"ぶたざるポケモン", type:"格闘", takasa:0.5, omosa:28.0, setumei:"木の上で群れを作って暮らす。群れからはぐれたマンキーは寂しくてすぐに怒り出す。" },
+  { id:3, code:79, name:"ヤドン",  bunrui:"まぬけポケモン", type:"エスパー，水", takasa:1.2, omosa:36.0, setumei:"動きが鈍く間抜け。叩かれても5秒経ってから痛さを感じるほどだ。" },
+  { id:4, code:83, name:"カモネギ",  bunrui:"かもがるポケモン", type:"ノーマル，飛行", takasa:0.8, omosa:15.0, setumei:"クキがなくなると生きていけない。だからクキを狙う相手とは命懸けで戦うのだ。" },
+  { id:5, code:100, name:"ビリリダマ",  bunrui:"ボールポケモン", type:"電気", takasa:0.5, omosa:10.4, setumei:"転がって移動するので地面がデコボコだとショックで爆発してしまう。" },
+  { id:6, code:101, name:"マルマイン",  bunrui:"ボールポケモン", type:"ノーマル，飛行", takasa:1.2, omosa:66.6, setumei:"電気エネルギーを溜め込むほど高速で動けるようになるがその分爆発しやすい。" },
+  { id:7, code:111, name:"サイホーン",  bunrui:"とげとげポケモン", type:"地面，岩", takasa:1.0, omosa:115.0, setumei:"半径10キロメートルの範囲を縄張りにしているが忘れてしまうらしい。" },
+  { id:8, code:129, name:"コイキング",  bunrui:"さかなポケモン", type:"水", takasa:0.9, omosa:10.0, setumei:"力のない情けないポケモン。たまに高く飛び跳ねても2メートルを越すのがやっとだ。" },
+  { id:9, code:132, name:"メタモン",  bunrui:"へんしんポケモン", type:"ノーマル", takasa:0.3, omosa:4.0, setumei:"変身は完璧なのだが笑わされて力が抜けると変身は解けてしまう" },
+  { id:10, code:129, name:"コイキング",  bunrui:"さかなポケモン", type:"水", takasa:0.9, omosa:10.0, setumei:"力のない情けないポケモン。たまに高く飛び跳ねても2メートルを越すのがやっとだ。" },
+];
+
+
 
 app.get("/keiyo2", (req, res) => {
   // 本来ならここにDBとのやり取りが入る
@@ -76,6 +90,70 @@ app.get("/keiyo_add", (req, res) => {
   let newdata = { id: id, code: code, name: name };
   station.push( newdata );
   res.render('db1', { data: station });
+});
+
+app.get("/pokemonzukan", (req, res) => {
+  // 本来ならここにDBとのやり取りが入る
+  res.render('pokemonzukan', {data: pokemon} );
+});
+
+// Create
+app.get("/pokemonzukan/create", (req, res) => {
+  res.redirect('/public/pokemonzukan_new.html');
+});
+
+// Read
+app.get("/pokemonzukan/:number", (req, res) => {
+  // 本来ならここにDBとのやり取りが入る
+  const number = req.params.number;
+  const detail = pokemon[ number ];
+  res.render('pokemonzukan_detail', {id: number, data: detail} );
+});
+
+//edit
+app.get("/pokemonzukan/edit/:number", (req, res) => {
+  // 本来ならここにDBとのやり取りが入る
+  const number = req.params.number;
+  const detail = pokemon[ number ];
+  res.render('pokemonzukan_edit', {id: number, data: detail} );
+});
+
+// Update
+app.post("/pokemonzukan/update/:number", (req, res) => {
+  // 本来は変更する番号が存在するか，各項目が正しいか厳重にチェックする
+  // 本来ならここにDBとのやり取りが入る
+  pokemon[req.params.number].code = req.body.code;
+  pokemon[req.params.number].name = req.body.name;
+  pokemon[req.params.number].bunrui = req.body.bunrui;
+  pokemon[req.params.number].type = req.body.type;
+  pokemon[req.params.number].takasa = req.body.takasa;
+  pokemon[req.params.number].omosa = req.body.omosa;
+  pokemon[req.params.number].setumei = req.body.setumei;
+  console.log( pokemon );
+  res.redirect('/pokemonzukan' );
+});
+
+app.post("/pokemonzukan/add", (req, res) => {
+  let code = req.body.code;
+  let name = req.body.name;
+  let bunrui = req.body.bunrui;
+  let type = req.body.type;
+  let takasa = req.body.takasa;
+  let omosa = req.body.omosa;
+  let setumei = req.body.setumei;
+  let id = pokemon.length + 1;
+  let newdata = { 
+    id: id, 
+    code: code, 
+    name: name, 
+    bunrui: bunrui, 
+    type: type, 
+    takasa: takasa, 
+    omosa: omosa, 
+    setumei: setumei 
+  };
+  pokemon.push( newdata );
+  res.render('pokemonzukan', { data: pokemon });
 });
 
 app.get("/hello1", (req, res) => {
